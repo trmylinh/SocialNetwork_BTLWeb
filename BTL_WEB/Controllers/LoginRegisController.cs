c5fd134b43d88c2aed5826d1fb081def2d4277a8
Tran My Linh <linhninh2@gmail.com>
2023-03-13 21:32:44 +0700

    init

---
+++ BTL_WEB/Controllers/LoginRegisController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BTL_WEB.Controllers
+{
+	public class LoginRegisController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Login()
+		{
+			return View();
+		}
+	}
+}
