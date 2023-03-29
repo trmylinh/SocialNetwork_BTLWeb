b3753b7e722643a92762a25ce57af226abb12ce7
trmylinh <linhninh2@gmail.com>
2023-03-29 13:50:27 +0700

    add router logout

---
+++ BTL_WEB/Controllers/PostController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BTL_WEB.Controllers
+{
+    public class PostController : Controller
+    {
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
