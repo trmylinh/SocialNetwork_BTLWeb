20ce48ddb688ffc13dcc61df13669357a4c79f5b
DanzyH <nguyenviethoang8902@gmail.com>
2023-04-03 22:58:18 +0700

    fixed connection to database

--- BTL_WEB/Program.cs
+++ BTL_WEB/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<SocialMediaContext>(x => x.UseSqlServer(
+builder.Services.AddDbContext<SocialMediaContext>(option => option.UseSqlServer(
     builder.Configuration.GetConnectionString("SocialMediaContext")
 ));
 builder.Services.AddSession();
